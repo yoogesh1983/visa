650a9efe89bdd8b20ab4d463919de4bf2e818b1d
yoogesh sharma <35018173+yoogesh1983@users.noreply.github.com>
2021-02-02 21:10:46 -0500

    Drools version of sql join

--- src/main/java/com/yoogesh/visa/Run.java
+++ src/main/java/com/yoogesh/visa/Run.java
@@ -7,6 +7,7 @@ import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
 import com.yoogesh.visa.model.Passport;
+import com.yoogesh.visa.model.VisaApplication;
 import com.yoogesh.visa.repository.ApplicationRepository;
 
 public class Run {
@@ -14,10 +15,13 @@ public class Run {
 	public static void main(String[] args) {
 		
 	    List<Passport> passports = ApplicationRepository.getPassports();
+	    List<VisaApplication> visaApplications = ApplicationRepository.getVisaApplications();
 
 	    KieContainer kieContainer = KieServices.Factory.get().getKieClasspathContainer();
 	    KieSession ksession = kieContainer.newKieSession("StatefulPassportValidation");
+	    
 	    passports.forEach(ksession::insert);
+	    visaApplications.forEach(ksession::insert);
 	    
 	    System.out.println("==== DROOLS SESSION END ==== ");
 	    ksession.fireAllRules();
@@ -25,9 +29,14 @@ public class Run {
 	    System.out.println("==== DROOLS SESSION END ==== ");
 	    
 	    System.out.println("==== PASSPORTS AFTER DROOLS SESSION === ");
-	    passports.forEach(passport -> {
-	      System.out.println(passport + " verdict: " + passport.getValidation() + ("" != passport.getCause() ? ", cause: " + passport.getCause() : ""));
-	    });
+	    
+	    
+	    passports.forEach(passport -> System.out.println(passport + " verdict: " + passport.getValidation()));
+
+	    System.out.println("==== APPLICATIONS STATE AFTER DROOLS SESSION === ");
+	    visaApplications.forEach(visaApplication ->
+	        System.out.println(visaApplication + " verdict: " + visaApplication.getValidation())
+	    );
 	}
 
 }
--- src/main/java/com/yoogesh/visa/repository/ApplicationRepository.java
+++ src/main/java/com/yoogesh/visa/repository/ApplicationRepository.java
@@ -6,46 +6,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.yoogesh.visa.model.Passport;
-
+import com.yoogesh.visa.model.VisaApplication;
 
 public class ApplicationRepository {
 
-  public static List<Passport> getPassports() {
-    List<Passport> passports = new ArrayList<>();
+	private static final String YOOGESH_PASSPORT_NUMBER = "CA-YOOGESH-1";
+	private static final String SUSHILA_PASSPORT_NUMBER = "CA-SUSHILA-2";
+	private static final String KRISTY_PASSPORT_NUMBER = "AU-KRISTY-3";
+	private static final String KRISHA_PASSPORT_NUMBER = "AU-KRISHA-4";
 
-    passports.add(Passport.newBuilder()
-      .withPassportNumber("CA-YOOGESH-1")
-      .withName("Yoogesh Sharma")
-      .withUnusedVisaPages(1)
-      .withExpiresOn(LocalDate.of(2017, Month.DECEMBER, 17))
-      .withAge(50)
-      .build());
+	public static List<Passport> getPassports() {
+		List<Passport> passports = new ArrayList<>();
 
-    passports.add(Passport.newBuilder()
-      .withPassportNumber("CA-SUSHILA-2")
-      .withName("Sushila Sapkota")
-      .withUnusedVisaPages(0)
-      .withExpiresOn(LocalDate.of(2045, Month.MAY, 11))
-      .withAge(12)
-      .build());
+		passports.add(Passport.newBuilder().withPassportNumber(YOOGESH_PASSPORT_NUMBER).withName("Yoogesh Sharma")
+				.withUnusedVisaPages(1).withExpiresOn(LocalDate.of(2017, Month.DECEMBER, 17)).withAge(50).build());
 
-    passports.add(Passport.newBuilder()
-      .withPassportNumber("AU-KRISTY-3")
-      .withName("Kristy Sharma")
-      .withUnusedVisaPages(20)
-      .withExpiresOn(LocalDate.of(2047, Month.NOVEMBER, 25))
-      .withAge(16)
-      .build());
+		passports.add(Passport.newBuilder().withPassportNumber(SUSHILA_PASSPORT_NUMBER).withName("Sushila Sapkota")
+				.withUnusedVisaPages(0).withExpiresOn(LocalDate.of(2045, Month.MAY, 11)).withAge(12).build());
 
-    passports.add(Passport.newBuilder()
-      .withPassportNumber("AU-KRISHA-4")
-      .withName("Krisha Sharma")
-      .withUnusedVisaPages(10)
-      .withExpiresOn(LocalDate.of(2045, Month.APRIL, 10))
-      .withAge(17)
-      .build());
+		passports.add(Passport.newBuilder().withPassportNumber(KRISTY_PASSPORT_NUMBER).withName("Kristy Sharma")
+				.withUnusedVisaPages(20).withExpiresOn(LocalDate.of(2047, Month.NOVEMBER, 25)).withAge(16).build());
 
-    return passports;
-  }
+		passports.add(Passport.newBuilder().withPassportNumber(KRISHA_PASSPORT_NUMBER).withName("Krisha Sharma")
+				.withUnusedVisaPages(10).withExpiresOn(LocalDate.of(2045, Month.APRIL, 10)).withAge(17).build());
+
+		return passports;
+	}
+
+	public static List<VisaApplication> getVisaApplications() {
+		List<VisaApplication> visaApplications = new ArrayList<>();
+
+		visaApplications.add(VisaApplication.newBuilder().withApplicationId(1)
+				.withPassportNumber(YOOGESH_PASSPORT_NUMBER).withVisitStartDate(LocalDate.of(2039, Month.DECEMBER, 27))
+				.withVisitEndDate(LocalDate.of(2040, Month.JANUARY, 4)).build());
+
+		visaApplications.add(VisaApplication.newBuilder().withApplicationId(2)
+				.withPassportNumber(SUSHILA_PASSPORT_NUMBER).withVisitStartDate(LocalDate.of(2039, Month.DECEMBER, 27))
+				.withVisitEndDate(LocalDate.of(2039, Month.JANUARY, 4)).build());
+
+		visaApplications.add(VisaApplication.newBuilder().withApplicationId(3)
+				.withPassportNumber(KRISTY_PASSPORT_NUMBER).withVisitStartDate(LocalDate.of(2044, Month.JANUARY, 1))
+				.withVisitEndDate(LocalDate.of(2044, Month.MARCH, 31)).build());
+
+		visaApplications.add(VisaApplication.newBuilder().withApplicationId(4)
+				.withPassportNumber(KRISHA_PASSPORT_NUMBER).withVisitStartDate(LocalDate.of(2045, Month.JANUARY, 1))
+				.withVisitEndDate(LocalDate.of(2045, Month.MARCH, 10)).build());
+
+		return visaApplications;
+	}
 
 }
